60a05cb373abaabcac635ca0ec44a8c8366fba31
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-07-19 12:16:09 -0300

    update

---
+++ substitua/js/script.js
@@ -0,0 +1,16 @@
+let resposta = document.getElementById('resposta')
+
+function res() {
+    let answer = prompt('Digite sua resposta:')
+
+    if (answer === null) {
+        return; // encerra a função se a resposta for 'null'
+    }
+    if (answer === 'pv' || answer === 'pv ') {
+        alert('Parabéns!! Você acertou!!!')
+    } else if (answer === '') {
+        alert('pq não preencheu???');
+    } else {
+        alert('eRRRRRRRRRRou pense mais');
+    }
+}
